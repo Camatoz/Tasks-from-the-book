fdf8c6912ed63fbaf32042c15b97d01b078e0fa4
Camatoz <yurabondik99@gmail.com>
2019-11-05 16:02:59 +0300

    fixed commit

--- Chapter-7/main.js
+++ Chapter-7/main.js
@@ -1,4 +1,4 @@
-let words = ["программа", "макака", "прекрасный", "оладушек"];
+let words = ["кот", "жук", "ухо", "мак"];
 
 // Выбираем случайное слово
 let word = words[Math.floor(Math.random() * words.length)];
@@ -10,38 +10,41 @@ for (var i = 0; i < word.length; i++) {
 }
 
 let remainingLetters = word.length;
-let guesses = 20;
+let guesses = 0;
 
 // Игровой цикл
-while (remainingLetters > 0 && guesses > 0) {
+while (remainingLetters > 0 && guesses < 6) {
   // Показываем состояние игры
   alert(answerArray.join(" "));
 
   // Запрашиваем вариант ответа
   let guess = prompt("Угадайте букву, или нажмите Отмена для выхода из игры.");
+
   if (guess === null) {
     // Выходим из игрового цикла
     break;
   } else if (guess.length !== 1) {
     alert("Пожалуйста, введите одиночную букву.");
   } else {
-    guesses--;
     guess = guess.toLowerCase();
-    // Обновляем состояние игры
-    for (var j = 0; j < word.length; j++) {
-      if (word[j] === guess && answerArray[j] === "_") {
-        answerArray[j] = guess;
-        remainingLetters--;
-      }
+    guesses++;
+    console.log(guesses);
+  }
+  // Обновляем состояние игры
+  for (var j = 0; j < word.length; j++) {
+    if (word[j] === guess && answerArray[j] === "_") {
+      answerArray[j] = guess;
+      remainingLetters--;
     }
   }
-  // Конец игрового цикла
 }
+// Конец игрового цикла
 
 // Отображаем ответ и поздравляем игрока
-alert(answerArray.join(" "));
-if (guesses > 0) {
-  alert("Отлично! Было загадано слово " + word);
-} else {
+
+if (remainingLetters > 0) {
   alert("Очень плохо! Было загадано слово " + word);
+} else {
+  alert("Отлично! Было загадано слово " + word);
 }
+alert(answerArray.join(" "));
