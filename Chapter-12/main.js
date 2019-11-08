35a6078194175aebaf2d7e5129c2331cbec57f37
Camatoz <yurabondik99@gmail.com>
2019-11-08 15:33:56 +0300

    Start 'ООП'

---
+++ Chapter-12/main.js
@@ -0,0 +1,45 @@
+var Car = function (x, y) {
+    this.x = x;
+    this.y = y;
+    this.draw();
+};
+Car.prototype.draw = function () {
+    var carHtml = '<img src="images.png">';
+    this.carElement = $(carHtml);
+    this.carElement.css({
+        position: "absolute",
+        left: this.x,
+        top: this.y
+    });
+    $("body").append(this.carElement);
+};
+Car.prototype.moveRight = function () {
+    this.x += 5;
+    this.carElement.css({
+        left: this.x,
+        top: this.y
+    });
+};
+Car.prototype.moveLeft = function () {
+    this.x -= 5;
+    this.carElement.css({
+        left: this.x,
+        top: this.y
+    });
+};
+Car.prototype.moveUp = function () {
+    this.y -= 5;
+    this.carElement.css({
+        left: this.x,
+        top: this.y
+    });
+};
+Car.prototype.moveDown = function () {
+    this.y += 55;
+    this.carElement.css({
+        left: this.x,
+        top: this.y
+    });
+};
+var tesla = new Car(20, 20);
+var nissan = new Car(100, 200);
